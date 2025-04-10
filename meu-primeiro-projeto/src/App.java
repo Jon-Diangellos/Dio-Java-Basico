7521377a3a837028463764edca860e166987dc6a
Jon-Diangellos <jonathasdiangellos@gmail.com>
2025-04-10 12:40:42 -0300

    gg

--- meu-primeiro-projeto/src/App.java
+++ meu-primeiro-projeto/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem Vindos! turma");
+        System.out.println("Bem Vindos! sxxturma");
     }
 }
